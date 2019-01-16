f32f5f473b519303c7f4ef12249c315e0279f70e
Amit Suneja <amitsuneja.devops@gmail.com>
2019-01-17 01:57:48 +0530

    Delete snap_backup_AMDC1AXHHA01.sh

--- snap_backup_AMDC1AXHHA01.sh
+++
@@ -1,7 +0,0 @@
-reggrp=prod_rg
-az account set --subscription "subscriptionID"
-az snapshot create -g $resgrp -n dev_sda_HOSTNAME_snapshot26Nov2018 --source dev_sda-HOSTNAME
-az snapshot create -g $resgrp -n dev_sdb_HOSTNAME_snapshot26Nov2018 --source dev_sdb-HOSTNAME
-az snapshot create -g $resgrp -n dev_sdc_HOSTNAME_snapshot26Nov2018 --source dev_sdc-HOSTNAME
-az snapshot create -g $resgrp -n dev_sdd_HOSTNAME_snapshot26Nov2018 --source dev_sdd-HOSTNAME
-az snapshot create -g $resgrp -n dev_sde_HOSTNAME_snapshot26Nov2018 --source dev_sde-HOSTNAME
